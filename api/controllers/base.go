d7c209e64af3117d76d45605ce99153d8b9956ee
Mateus Fortunato <mateusfortunato00@gmail.com>
2021-05-11 16:36:59 -0300

    fix: change from echo to gorilla mux

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -3,16 +3,18 @@ package controllers
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/0xfortunato/freak/api/models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/labstack/echo"
+
+	"github.com/gorilla/mux"
 )
 
 type Server struct {
 	DB     *gorm.DB
-	Router *echo.Echo
+	Router *mux.Router
 }
 
 func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
@@ -34,7 +36,12 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 	// database migration
 	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{})
 
-	server.Router = echo.New()
+	server.Router = mux.NewRouter()
 
 	server.initializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
