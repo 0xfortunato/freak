55cb1338c39510dab98e17b3dc4efd9929d4d3e2
Mateus Fortunato <mateusfortunato00@gmail.com>
2021-05-16 21:08:13 -0300

    feat: seeder for database populate

---
+++ api/seed/seeder.go
@@ -0,0 +1,68 @@
+package seed
+
+import (
+	"log"
+
+	"github.com/0xfortunato/freak/api/models"
+
+	"github.com/jinzhu/gorm"
+)
+
+var users = []models.User{
+	models.User{
+		Nickname: "em",
+		Email:    "fakeemail@gmail.com",
+		Password: "123",
+	},
+	{
+		Nickname: "nas",
+		Email:    "anotheremail@gmail.com",
+		Password: "321",
+	},
+	{
+		Nickname: "cube",
+		Email:    "cubesemail@gmail.com",
+		Password: "cub3",
+	},
+}
+
+var posts = []models.Post{
+	models.Post{
+		Title:   "Title 1",
+		Content: "Hello world 1",
+	},
+	models.Post{
+		Title:   "Title 2",
+		Content: "Hello world 2",
+	},
+}
+
+func Load(db *gorm.DB) {
+
+	err := db.Debug().DropTableIfExists(&models.Post{}, &models.User{}).Error
+	if err != nil {
+		log.Fatalf("cannot drop table: %v", err)
+	}
+	err = db.Debug().AutoMigrate(&models.User{}, &models.Post{}).Error
+	if err != nil {
+		log.Fatalf("cannot migrate table: %v", err)
+	}
+
+	err = db.Debug().Model(&models.Post{}).AddForeignKey("author_id", "users(id)", "cascade", "cascade").Error
+	if err != nil {
+		log.Fatalf("attaching foreign key error: %v", err)
+	}
+
+	for i := range users {
+		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed users table: %v", err)
+		}
+		posts[i].AuthorID = users[i].ID
+
+		err = db.Debug().Model(&models.Post{}).Create(&posts[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed posts table: %v", err)
+		}
+	}
+}
